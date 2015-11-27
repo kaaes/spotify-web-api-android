39204cf0238a6e2797c0b022b897f1aefe98d733
Kasia Drzyzga <kasia@drzyzga.pl>
2015-11-27 12:29:32 +0100

    Use MVP

--- app/src/main/java/com/spotify/sdk/android/MainActivity.java
+++ app/src/main/java/com/spotify/sdk/android/MainActivity.java
@@ -1,53 +1,29 @@
 package com.spotify.sdk.android;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.SearchView;
-import android.widget.Toast;
 
-import kaaes.spotify.webapi.android.SpotifyApi;
-import kaaes.spotify.webapi.android.SpotifyService;
+import java.util.List;
+
 import kaaes.spotify.webapi.android.models.Track;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
+public class MainActivity extends AppCompatActivity implements Search.View {
 
     static final String EXTRA_TOKEN = "EXTRA_TOKEN";
     private static final String KEY_CURRENT_QUERY = "CURRENT_QUERY";
 
-    private RecyclerView mResultsList;
-    private SearchView mSearchView;
-    private SearchResultsAdapter mAdapter;
-    private SpotifyService mSpotifyService;
-    private String mCurrentQuery;
-    private Player mPlayer;
-
-    private ServiceConnection mServiceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            mPlayer = ((PlayerService.PlayerBinder) service).getService();
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            mPlayer = null;
-        }
-    };
+    private Search.ActionListener mActionListener;
 
     private LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
     private ScrollListener mScrollListener = new ScrollListener(mLayoutManager);
-    ;
+    private SearchResultsAdapter mAdapter;
+
 
     private class ScrollListener extends ResultListScrollListener {
 
@@ -57,8 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onLoadMore() {
-            Log.d(TAG, "Load more...");
-            mAdapter.getNextPage();
+            mActionListener.loadMoreResults();
         }
     }
 
@@ -71,45 +46,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState != null) {
-            mCurrentQuery = savedInstanceState.getString(KEY_CURRENT_QUERY);
-        }
-
-        mSearchView = (SearchView) findViewById(R.id.search_view);
-        mResultsList = (RecyclerView) findViewById(R.id.search_results);
-        mResultsList.setHasFixedSize(true);
-
         Intent intent = getIntent();
         String token = intent.getStringExtra(EXTRA_TOKEN);
 
-        initApiClient(token);
-        setupSearch();
-        setupPlayer();
+        mActionListener = new SearchPresenter(this, this);
+        mActionListener.init(token);
 
-        if (mCurrentQuery != null) {
-            search(mCurrentQuery);
-        }
-    }
-
-    private void initApiClient(String token) {
-        SpotifyApi spotifyApi = new SpotifyApi();
-
-        if (token != null) {
-            spotifyApi.setAccessToken(token);
-        } else {
-            logError("No valid access token");
-        }
-
-        logMessage("Api Client created");
-        mSpotifyService = spotifyApi.getService();
-    }
-
-    private void setupSearch() {
-        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        // Setup search field
+        SearchView searchView = (SearchView) findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                logMessage("query text submit " + query);
-                search(query);
+                mActionListener.search(query);
                 return true;
             }
 
@@ -119,87 +67,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mAdapter = new SearchResultsAdapter(this, mSpotifyService, new SearchResultsAdapter.ItemSelectedListener() {
+
+        // Setup search results list
+        mAdapter = new SearchResultsAdapter(this, new SearchResultsAdapter.ItemSelectedListener() {
             @Override
             public void onItemSelected(View itemView, Track item) {
-                handleTrackSelection(item);
+                mActionListener.selectTrack(item);
                 itemView.setSelected(true);
                 itemView.setActivated(true);
             }
         });
 
-        mResultsList.setLayoutManager(mLayoutManager);
-        mResultsList.setAdapter(mAdapter);
-        mResultsList.addOnScrollListener(mScrollListener);
-    }
+        RecyclerView resultsList = (RecyclerView) findViewById(R.id.search_results);
+        resultsList.setHasFixedSize(true);
+        resultsList.setLayoutManager(mLayoutManager);
+        resultsList.setAdapter(mAdapter);
+        resultsList.addOnScrollListener(mScrollListener);
 
-    private void search(@NonNull String query) {
-        if (!query.isEmpty() && !query.equals(mCurrentQuery)) {
-            mCurrentQuery = query;
-            mScrollListener.reset();
-            mAdapter.searchTracks(query, 20);
+        // If Activity was recreated wit active search restore it
+        if (savedInstanceState != null) {
+            String currentQuery = savedInstanceState.getString(KEY_CURRENT_QUERY);
+            mActionListener.search(currentQuery);
         }
     }
 
-    private void handleTrackSelection(Track track) {
-        String previewUrl = track.preview_url;
-
-        if (previewUrl == null) {
-            logMessage("Track doesn't have a preview");
-            return;
-        }
-
-        if (mPlayer == null) return;
-
-        String currentTrackUrl = mPlayer.getCurrentTrack();
-
-        if (currentTrackUrl == null || !currentTrackUrl.equals(previewUrl)) {
-            mPlayer.play(previewUrl);
-        } else if (mPlayer.isPlaying()) {
-            mPlayer.pause();
-        } else {
-            mPlayer.resume();
-        }
+    @Override
+    public void reset() {
+        mScrollListener.reset();
+        mAdapter.clearData();
     }
 
-    private void setupPlayer() {
-        bindService(PlayerService.getIntent(this), mServiceConnection, BIND_AUTO_CREATE);
+    @Override
+    public void addData(List<Track> items) {
+        mAdapter.addData(items);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        startService(PlayerService.getIntent(this));
+        mActionListener.pause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        stopService(PlayerService.getIntent(this));
+        mActionListener.resume();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if (mCurrentQuery != null) {
-            outState.putString(KEY_CURRENT_QUERY, mCurrentQuery);
+        if (mActionListener.getCurrentQuery() != null) {
+            outState.putString(KEY_CURRENT_QUERY, mActionListener.getCurrentQuery());
         }
     }
 
     @Override
     protected void onDestroy() {
-        unbindService(mServiceConnection);
+        mActionListener.destroy();
         super.onDestroy();
-    }
-
-    private void logError(String msg) {
-        Toast.makeText(this, "Error: " + msg, Toast.LENGTH_SHORT).show();
-        Log.e(TAG, msg);
-    }
-
-    private void logMessage(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, msg);
     }
 
 }
